e15bc2026281d5997c56536eb83e41cbe1d3ca4d
LeamanB <LeamanBrown@gmail.com>
2022-12-02 10:52:35 -0600

    class

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import html from "html-literal";
-
-<footer>
-&copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
-        &copy; 
-        </footer>
+export default () => html`
+  <footer>
+    &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
+  </footer>
+`;
